7e4fa18594128737413033ba6f4f3f2a9487842b
karl07 <1474402804@qq.com>
2019-03-10 23:52:16 +0800

    Hello World

---
+++ Assets/Scripts/ButtonTry.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTry : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+
+	public void OnButtonClick()
+	{
+		gameObject.SetActive(!gameObject.activeSelf);
+	}
+
+}
