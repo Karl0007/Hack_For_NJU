59cb12a8150686b7edd48a62ffce9442445728a7
karl07 <1474402804@qq.com>
2019-03-11 14:01:12 +0800

    add Nav

---
+++ Assets/Scripts/NavTry.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavTry : MonoBehaviour
+{
+	public Transform tr;
+	public NavMeshAgent nag;
+    // Start is called before the first frame update
+    void Start()
+    {
+		nag = gameObject.GetComponent<NavMeshAgent>();
+		Debug.Log(nag.gameObject.name);
+    }
+
+	public void GoDes()
+	{
+		nag.SetDestination(tr.position);
+
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+    }
+}
